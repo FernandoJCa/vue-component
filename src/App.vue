81015bf36158a5a3c898d7ad2e4966570eb43b7e
Fernando Cardozo <cardozof123@outlook.com>
2024-09-02 15:22:48 -0500

    Complete demo

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import ButtonDemo from './components/ButtonDemo.vue'
+</script>
+
+<template>
+  <div class="min-h-screen bg-gray-100 flex items-center justify-center">
+    <ButtonDemo />
+  </div>
+</template>
+
+<style>
+@import '@/assets/index.css';
+</style>
